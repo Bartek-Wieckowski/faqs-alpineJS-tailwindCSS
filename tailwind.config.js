7bd26ec8c7d13048038373f08317b35b534ca40b
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-09-25 08:33:46 +0200

    create html,styles,functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "t-very-dark-desaturated-blue": "hsl(238, 29%, 16%)",
+        "t-soft-red": "hsl(14, 88%, 65%)",
+        "soft-violet": "hsl(273, 75%, 66%)",
+        "soft-blue": "hsl(240, 73%, 65%)",
+      },
+      height: {
+        ownHeightDesktop: "500px",
+        ownHeightMobile: "650px",
+      },
+      maxWidth: {
+        "2.5xl": "720px",
+      },
+      fontFamily: {
+        "Kumbh-Sans": ['"Kumbh Sans"'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
